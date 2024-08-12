ada1db04ab90fcb050cbc3bc12fe92c21fa43bdb
mizan21 <mizanorrahman398@gmail.com>
2024-08-12 22:48:24 +0600

    updated part

---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner lg:container lg:mx-auto rounded-xl">
+      <div className=" py-[200px] px-[150px] lg:max-w-xl  text-white">
+        <h1 className="text-5xl font-bold py-5">
+          Affordable Price For Car Servicing
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,5 +1,39 @@
+import logo from "../../../assets/genius_car/logo.svg";
 const Footer = () => {
-  return <div>this is footer!</div>;
+  return (
+    <div className="bg-black ">
+      <footer className="footer  mx-5 lg:container lg:mx-auto p-10 text-white">
+        <aside>
+          <img src={logo} alt="genius logo" />
+          <p>
+            ACME Industries Ltd.
+            <br />
+            Providing reliable tech since 1992
+          </p>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
